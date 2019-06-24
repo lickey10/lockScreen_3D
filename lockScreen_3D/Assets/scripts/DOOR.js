6f99a02129b4fa86f13eb5b4693f7e887478b5ca
lickey10 <lickey10@gmail.com>
2019-06-24 09:52:37 -0600

    first import

---
+++ lockScreen_3D/Assets/scripts/DOOR.js
@@ -0,0 +1,33 @@
+﻿var doorClip : AnimationClip;
+var Key : GameObject;
+private var Door = false;
+
+function Start () 
+{
+
+}
+
+function Update () 
+{
+	if (Input.GetKeyDown(KeyCode.E) && Door == true && Key.active == false)
+	{
+		//GameObject.Find("leftGate").animation.Play("gateOpenLeft");
+		gameObject.GetComponent.<Animation>().Play(doorClip.name);
+	}
+}
+
+function OnTriggerEnter (theCollider : Collider)
+{
+	if (theCollider.tag == "Player")
+	{
+		Door = true;
+	}
+}
+
+function OnTriggerExit (theCollider : Collider)
+{
+	if (theCollider.tag == "Player")
+	{
+		Door = false;
+	}
+}
